ae492f4253434607a9b860aa2307237a0ad2f40b
Angel David Rodriguez Vanegas <angeldarova@gmail.com>
2026-08-05 20:09:00 -0500

    Bosquejo de clase inventario finalizado

--- unidad_4/proyecto_final/models/venta.py
+++ unidad_4/proyecto_final/models/venta.py
@@ -4,3 +4,15 @@ class Venta:
          Constructor de la clase Venta
         """
         pass
+    
+    def calcular_total(self):
+        """
+        Metodo calcular el total de la venta
+        """
+        print("Metodo calcular_total() pendiente")
+        
+    def mostrar(self):
+        """
+            Metodo mostrar los detalles de la venta
+        """
+        print("Metodo mostrar() pendiente")
