4d60fcc0a5f520965abb5a83d0af554cca011e68
Angel David Rodriguez Vanegas <angeldarova@gmail.com>
2026-08-05 20:14:48 -0500

    bosquejo de clase ventas finalizado

---
+++ unidad_4/proyecto_final/services/ventas.py
@@ -0,0 +1,18 @@
+class Ventas:
+    def __init__(self):
+        """
+            Decidi almacenar en una lista ya que me interesa mantener registro y orden cronologico
+            y en orden de llegada cada venta realizada
+        """
+        
+    def registrar_venta(self):
+        print("\n[Ventas]")
+        print("Registrar venta (pendiente)")
+        
+    def listar_ventas(self):
+        print("\n[Ventas]")
+        print("Listar ventas (pendiente)")
+        
+    def total_vendido(self):
+        print("\n[Ventas]")
+        print("Calcular total vendido (pendiente)")
