d5ef75b48f20a2102361162f2c74475fd879aad6
Angel David Rodriguez Vanegas <angeldarova@gmail.com>
2026-08-10 21:04:11 -0500

    clase inventario testeada y finalizada con exito

--- unidad_4/proyecto_final/main.py
+++ unidad_4/proyecto_final/main.py
@@ -4,42 +4,44 @@ from services.ventas import Ventas
 from models.producto import Producto
 
 def main():
-    
-    #Dependencias de la clase main
+
+    # Dependencias de la clase main
     inventario = Inventario() #instanciamos la clase inventario
-    ventas = Ventas() #Instanciamos la clase ventas
-    
+    ventas = Ventas() # instanciamos la clase ventas
+
     while True:
         opcion = mostrar_menu()
-        
-        if opcion == "1":
+
+        if opcion ==  "1":
             inventario.registrar_producto()
-            
+
         elif opcion == "2":
-            inventario.listar_producto()
-            
+            inventario.listar_productos()
+
         elif opcion == "3":
             inventario.buscar_producto()
-            
+
         elif opcion == "4":
             inventario.eliminar_producto()
-        
+
         elif opcion == "5":
             ventas.registrar_venta()
-            
+
         elif opcion == "6":
             ventas.listar_ventas()
-            
+
         elif opcion == "7":
             ventas.total_vendido()
-            
+
         elif opcion == "8":
-            print("\nGracias por utilizar el sistema")
+            print("\nGracias por utilizar el sistema.")
             break
-        
+
         else:
-            print("\nOpcion invalida.")
-            
+            print("\nOpción inválida.")
+
+
+"""
 producto = Producto(
     "P001",
     "teclado Logitech",
@@ -48,11 +50,13 @@ producto = Producto(
 )
 
 producto.mostrar()
+
 print("\n¿Hay 5 unidades disponibles?")
 print(producto.hay_stock(5))
 
 print("\n¿Hay 25 unidades disponibles?")
 print(producto.hay_stock(25))
+
 
 print("\nVendiendo 3 unidades...")
 resultado = producto.disminuir_stock(3)
@@ -61,9 +65,31 @@ if(resultado):
     print("Venta realizada")
 else:
     print("Venta no realizada")
-    
+
 producto.mostrar()
 
+"""
+
+inventario = Inventario()
+print("Se crean dos productos...")
+inventario.registrar_producto("P001", "teclado logitech", 18000, 10)
+inventario.registrar_producto("P002", "mouse logitech", 2000, 30)
+
+inventario.listar_productos()
+print("buscando el producto P002...")
+producto_encontrado= inventario.buscar_producto("P003")
+print(f"producto encontrado:")
+if producto_encontrado != None:
+    producto_encontrado.mostrar()
+else:
+    print("No se encontró el producto")
+
+
+print(f"cantidad de productos en lista: {inventario.mostrar_cantidad_productos()}")
+
+print("Eliminando el producto P002")
+inventario.eliminar_producto("P002")
+print(f"cantidad de productos en lista: {inventario.mostrar_cantidad_productos()}")
 
 #main()
-    
+
--- unidad_4/proyecto_final/services/inventario.py
+++ unidad_4/proyecto_final/services/inventario.py
@@ -1,34 +1,64 @@
 from models.producto import Producto
 
 class Inventario:
-    
+
+
     def __init__(self):
         """
-        Aqui represento el inventario en una estructura de lista porque me interesa el orden de insercion
+            Aqui represento el inventario en una estructra de lista, porque me interesa
+            el orden de inserción.
         """
-        self.productos = [] # Es clave no perder de vista el atributo de la clase
-        
-    def registrar_producto(self):
+        self.productos = [] # Es clave no perder de vista el atribbuto de la clase
+
+    def registrar_producto(self, codigo, nombre, precio, stock):
         """
-            Permite agregar productos al inventario
+            Permite agregar productos al inventario.
         """
-        
-        
-    def listar_producto(self):
-        
-        print("\n[Inventario]")
-        print("Listar productos (pendiente)")
-        
-    def buscar_producto(self):
-        print("\n[Inventario]")
-        print("Buscar producto (pendiente)")
-        
-    def eliminar_producto(self):
-        print("\n[Inventario]")
-        print("Eliminar producto (pendiente)")
-        
+        producto = Producto(codigo, nombre, precio, stock) # aqui creamos internamente el producto que agregaremos al inventario (stand).
+        self.productos.append(producto) # aqui agregamos el producto al inventario (lista)
+        print(f"El producto {producto.nombre} ha sido agregado exitosamente")
+
+    
+    def listar_productos(self):
+        """
+        Listar significa mostrar todo lo que hay en una lista.
+        """
+        if not self.productos: # Si No hay elementos en la lista self.productos
+            print("No hay productos registrados hasta el momento")
+            return
+        print("\nLISTADO DE PRODUCTOS")
+        print("-"*40)
+
+        for producto in self.productos:
+            producto.mostrar()
+            print("-"*40)
+
+
+    def buscar_producto(self, codigo):
+        for producto in self.productos:
+            if producto.codigo == codigo:
+                print("se encontró el producto!")
+                return producto
+
+        return None   
+
+    def eliminar_producto(self, codigo):
+        """
+        1. buscamos el producto
+        2. Si no existe (is None): producto no encontrado
+        3. Si existe, se carga en la variable producto y lo eliminamos con remove()
+        """
+        producto = self.buscar_producto(codigo)
+        if producto is None:
+            print("Producto no encontrado")
+            return
+        self.productos.remove(producto)
+        print("Producto eliminado exitosamente")
+
     def mostrar_cantidad_productos(self):
         """
-            Se puede implementar si necesitamos conocer cuantos productos tenemos en el inventario
+            Este método se puede implementar si necesitamos conocer cuantos productos tenemos en inventario.
         """
-        print("mostrar cantidad de productos pendiente")
+        return self.productos.__len__()
+
+    
